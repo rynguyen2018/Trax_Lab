5c30ee3a875dcf8ae363bd0554ba9737aefe7394
Ryan Nguyen <rnwin7@gmail.com>
2017-10-12 13:39:19 -0700

    interesting reads

--- Code/adpA-graph.R
+++ Code/adpA-graph.R
@@ -1,9 +1,10 @@
 library(ggplot2)
 
-setwd("C:/Users/Ryan/Desktop/gitcode/Trax_Lab/Code")
+#setwd("C:/Users/Ryan/Desktop/gitcode/Trax_Lab/Code")
+setwd("/Users/ryannguyen/Desktop/Trax_code/Code")
 #setwd("C:/Users/Death Star/Desktop/Trax_Lab/Code")
-points<- read.csv("adpA-experimental.csv", header=TRUE)
-time <- points$time
-ys <- points$IMV.intensity.Mean.Value.
+points<- read.csv("adpA-experimental_rev2.csv", header=TRUE)
+time <- points$Time
+ys <- points$Concentration_micromolar
 
-ggplot(points, aes(x=time, y=ys)) +geom_point() + geom_line()
+ggplot(points, aes(x=time, y=ys)) +geom_point() + geom_line()+ labs(x="Time(seconds)", y= "[adpA](micromolar)", title= "AdpA Concentration")
